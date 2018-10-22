e3ca45d5326faccf9d93f4fb07667c875078fff6
Andres Carrillo Lopez <andrescl@terpmail.umd.edu>
2018-10-21 20:21:38 -0400

    added close and remove issues functions

--- main.js
+++ main.js
@@ -1,24 +1,82 @@
-function fetchIssues(){
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
+
+function saveIssue(e){
+    var issueDesc = document.getElementById('issueDesInput').value;
+    var issueSeverity = document.getElementById('issueSevInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    //var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+    if(localStorage.getItem('issues') ==null){
+        var issues = [];
+    }else{
+        var issues = JSON.parse(localStorage.getItem('issues'));
+    }
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    //resseting input elements
+    document.getElementById('issueInputForm').reset();
+    //reloads issues
+    fetchIssues();
+    //prevent form from submitting
+    e.preventDefault();
+}
+
+function setStatusClosed(id) {
     var issues = JSON.parse(localStorage.getItem('issues'));
-    var issueList = document.getElementById('issuesList')
+    for(var j =0; j < issues.length; j++){
+        if(issues[j].id == id){
+            issues[j].status = 'Closed';
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
 
-    issueList.innerHTML = '';
+function deleteIssue(id){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    for(var j =0; j<issues.length;j++){
+        if(issues[j].id == id){
+            issues.splice(j,1);
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
+    fetchIssues();
+}
 
-    for(var i =0; i<issues.length; i++){
-        var id = issues[i].id;
-        var descr = issues[i].description;
-        var severity = issues[i].severity;
-        var assignedTo = issues[i].assignedTo;
-        var status = issues[i].status
+function fetchIssues(){
 
-        issueList.innerHTML += '<div class="well">'+
-                                '<h6>Issue ID: ' + id + '</h6>' +
-                                '<p><span class="label label-info">' +status+'</span></p>'+
-                                '<h3>'+desc+'</h3>'+
-                                '<p><span class="glyphicon glyphicon-time"></span>'+severity+'</p>'+
-                                '<p><span class= "glyphicon glyphico-user"></span>'+assignedTo+'</p>'+
-                                '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
-                                '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
-                                '</div>';
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesListe = document.getElementById('issuesList');
+  
+    issuesList.innerHTML = '';
+  
+    for (var i = 0; i < issues.length; i++) {
+      var id = issues[i].id;
+      var desc = issues[i].description;
+      var severity = issues[i].severity;
+      var assignedTo = issues[i].assignedTo;
+      var status = issues[i].status;
+  
+
+        issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
     }
 }
+
